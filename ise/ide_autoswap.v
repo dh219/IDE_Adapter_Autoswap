d1dcf58833ec65d3d0251ced7ddf1c1e2576a2db
David <dh219@aibs.org.uk>
2023-07-05 21:11:29 +0100

    Simplifing the verilog

--- ise/ide_autoswap.v
+++ ise/ide_autoswap.v
@@ -1,7 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Engineer: Copyright 2023 D Henderson
 // 
 // Create Date:    21:55:54 06/29/2023 
 // Design Name: 
@@ -15,7 +14,7 @@
 //
 // Revision: 
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments: Released under the terms of the GPL v2
 //
 //////////////////////////////////////////////////////////////////////////////////
 module ide_autoswap(
@@ -34,24 +33,15 @@ module ide_autoswap(
     );
 
 	reg [7:0] cmd;	
-	reg [7:0] lastcmd;
 	wire commandsend = { _CS, DA, _DIOW } == 6'b101110;
 	wire data = { _CS, DA } == 5'b10000;
-	wire swap = data && ( cmd != 8'hEC );
+	wire swap = data && ( cmd != 8'hEC ); // command 0xEC is Identify Drive. This is the only PIO data transfer that must remain unswapped
 
 	always @( posedge commandsend ) begin
-		lastcmd <= cmd;
 		cmd <= D[7:0];
 	end
 
-	wire [15:0] out = { 4'b0100, cmd[7:4], 4'b0100, cmd[3:0] };
-// cmd = 'hAC ? 
-// EC
-
-//	assign D = _DIOR ? 16'hzz : ( swap ? { DD[7:0], DD[15:8] } : DD );
 	assign D = _DIOR ? 16'hzz : ( swap ? { DD[7:0], DD[15:8] } : DD );
 	assign DD = _DIOW	? 16'hzz : ( swap ? { D[7:0], D[15:8] } : D );
-//	assign DD = _DIOW	? 16'hzz : D;
-
 
 endmodule
